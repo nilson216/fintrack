4aa034efe9666520e22ca343a91a6cfd9ab21f35
Nilson <netohjunior216@gmail.com>
2025-11-07 08:30:55 -0300

    feat: add password input with visibility change

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,3 +1,5 @@
+import { Eye, EyeOffIcon } from 'lucide-react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Button } from '@/components/ui/button'
@@ -12,8 +14,9 @@ import {
 import { Input } from '@/components/ui/input'
 
 const SignupPage = () => {
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
       <Card className="w-[500px] p-2">
         <CardHeader>
           <CardTitle>Crie a sua conta</CardTitle>
@@ -24,18 +27,32 @@ const SignupPage = () => {
           <Input type="text" placeholder="Digite seu nome" />
           <Input type="text" placeholder="Digite seu sobrenome" />
           <Input type="email" placeholder="Digite seu e-mail" />
-          <Input type="password" placeholder="Digite sua senha" />
+          <div className="relative">
+            <Input
+              type={passwordIsVisible ? 'text' : 'password'}
+              placeholder="Digite sua senha"
+            />
+            <Button
+              className="absolute bottom-0 right-0 top-0 my-auto mr-1 h-8 w-8 text-muted-foreground"
+              variant="ghost"
+              onClick={() => setPasswordIsVisible((prev) => !prev)}
+            >
+              {passwordIsVisible ? <EyeOffIcon /> : <Eye />}
+            </Button>
+          </div>
+        </CardContent>
+        <CardFooter>
           <Button type="submit" className="w-full">
             Criar Conta
           </Button>
-        </CardContent>
-        <CardFooter className="flex items-center justify-center">
-          <p className="text-center opacity-50">Já possui uma conta?</p>
-          <Button variant="link" asChild>
-            <Link to="/login">Faça Login</Link>
-          </Button>
         </CardFooter>
       </Card>
+      <div className="flex items-center justify-center">
+        <p className="text-center opacity-50">Já possui uma conta?</p>
+        <Button variant="link" asChild>
+          <Link to="/login">Faça Login</Link>
+        </Button>
+      </div>
     </div>
   )
 }
