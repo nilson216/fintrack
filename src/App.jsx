380a994cc30b39a3fbcf8d28f82651cc6412d2b3
Nilson <netohjunior216@gmail.com>
2025-11-06 22:27:19 -0300

    feat: update shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 const App = () => {
-  return <h1>Hello world</h1>
+  return (
+    <div className="Dark">
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default App
