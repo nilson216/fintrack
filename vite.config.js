f4ccf492ab204cec02f222f1095ba96142016bc0
Nilson <netohjunior216@gmail.com>
2025-12-08 18:18:24 -0300

    feat: add shadcn.png image and update Vite config for improved path resolution

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
